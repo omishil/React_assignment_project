f1a3b4194d4de3236690e3bd66ab2fe68655e1e6
omishil <omis52482u@example.com>
2026-09-13 21:47:16 +0600

    Complete Dev Stack project

--- src/Components/banner.tsx
+++ src/Components/banner.tsx
@@ -4,10 +4,10 @@ export default function Banner() {
     <>
     <div className="bannerSection mt-[50px]  w-[85%] h-[500px] flex flex-row m-auto justify-between">
     <div className="  text-black w-[600px] flex flex-col gap-[40px]">
-      <h1 className=" font-bold text-[60px] ">Build Your Ideal 
-<p className="fon bg-gradient-to-r from-[#EC3950] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+      <h1 className=" font-bold text-[60px] ">Build Your Ideal <br />
+<span className="fon bg-gradient-to-r from-[#EC3950] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
  Development Stack
-</p>        </h1>
+</span>        </h1>
       <p>
         Explore front-end, back-end, database, and tooling options, compare
         them side by side, and put together the stack that fits your next
@@ -15,7 +15,7 @@ export default function Banner() {
       </p>
       <div className="bannerBtn flex gap-[15px]">
       <button className="text-white rounded-[10px] px-4 py-2  bg-gradient-to-r from-[#F97316] to-[#7C3AED]">Explore Technologies</button>
-      <button>Learn more</button>
+      <button className="border border-gray-400 rounded-[10px] px-4 py-2">Learn more</button>
       </div>
     </div>
     <div className="rightImg">
